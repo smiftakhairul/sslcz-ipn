866dd094da398e5fdbb31d0473adb9747f02b18b
Sajan Poddar <sajan.chandra@sslwireless.com>
2020-04-07 17:17:34 +0600

    added config file in misc

--- config/misc.php
+++ config/misc.php
@@ -4,13 +4,13 @@ return [
 
     'firebase' => [
         'authorization_key' => env('FIREBASE_AUTH_KEY', ''),
-        'url' => env('FIREBASE_URL', 'https://fcm.googleapis.com/fcm/send'),
+        'url' => env('FIREBASE_URL', ''),
     ],
     'sms' => [
-        'url' => env('SMS_API_URL','https://sms.sslwireless.com/pushapi/dynamic/server.php'),
-        'user' => env('SMS_API_USER','easychkout'),
-        'password' => env('SMS_API_PASSWD','50?8D82e'),
-        'sid' => env('SMS_API_SID','EasyChkOut'),
+        'url' => env('SMS_API_URL',''),
+        'user' => env('SMS_API_USER',''),
+        'password' => env('SMS_API_PASSWD',''),
+        'sid' => env('SMS_API_SID',''),
     ],
     'notification' => [
         'delay_in_second' => env('NOTIFICATION_DELAY_SECOND',0)
