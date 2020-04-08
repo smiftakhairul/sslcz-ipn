18e280b7555b018c0e086de59e909053036971cf
Sajan Poddar <sajan.chandra@sslwireless.com>
2020-04-08 16:30:47 +0600

    timezone set

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
